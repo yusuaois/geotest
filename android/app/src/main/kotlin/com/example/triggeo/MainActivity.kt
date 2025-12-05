cf4e7d616024c929cd46cb0ad1f4a41507ec1de9
AnchialC <yusuaois@outlook.com>
2025-12-06 03:44:55 +0800

    manifest

--- android/app/src/main/kotlin/com/example/triggeo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/triggeo/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.example.triggeo
+package com.yourpackage.triggeo  // 替换为你的实际包名（如：com.example.triggeo）
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity(){}
+class MainActivity : FlutterFragmentActivity() {
+    // 无需额外代码，空类即可
+}
